26bb5503d0b27bef8012a4f50b293b190132e9a2
vamandeshmukh <vamandeshmukh@gmail.com>
2021-08-04 09:55:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com.db.javademo.aug2021
+ Java demo for DB grads Aug 2021
---
+++ src/com/db/javademo/aug2021/App.java
@@ -0,0 +1,10 @@
+package com.db.javademo.aug2021;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+	}
+
+}
